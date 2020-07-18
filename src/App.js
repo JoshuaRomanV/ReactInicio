9f7b9019d34b2ef3409fe1746de6e20889fb463c
willowy <vazquezjoshua47@gmail.com>
2020-07-18 01:06:46 -0500

    listas en react

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import CondicionalSection from './sections/condicional'
 
 
 function App() {
+  const numbers = [1, 1, 3, 4, 5]
     return (
       <div className="App">
-        <CondicionalSection />
+        <h4> Trabajando con listas</h4>
+        {numbers.map((number, index)=>{
+          return <p key={index}> Soy el numero {number}</p>
+        })} 
       </div>
     );
 }
