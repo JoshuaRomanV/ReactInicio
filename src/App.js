81341ba37cf2c950ca56722c4477e37a3cb1ebbf
willowy <vazquezjoshua47@gmail.com>
2020-08-16 18:30:32 -0500

    eventos

--- src/App.js
+++ src/App.js
@@ -2,35 +2,35 @@ import React, {Component} from 'react';
 import CondicionalSection from './sections/condicional'
 import cars from './data/cars.json'
 
-class CarItem extends Component{
-  render () {
-    const {car} = this.props
 
-    return (
-      <li>
-    <p><strong> Nombre: </strong> {car.name}</p>
-    <p><strong>Marca:</strong>{car.company}</p>
-    </li>
-    )
+class App extends Component {
+  constructor () {
+    super()
+    this.state = {mouseX: 0, mouseY: 0}
+    //this.handleMouseMove = this.handleMouseMove.bind(this)
   }
-}
-function App() {
+  handleMouseMove= (e) =>{
+    const {clientX, clientY} = e
+    this.setState({mouseX: clientX, mouseY: clientY })
+  }
+  handleClick (e) {
+    console.log(e) 
+    console.log(e.nativeEvent)
+    alert('Wachu whachu wa')
+  }
+  render ()  {
     return (
       <div>
         <h4> Trabajando con listas con objetos</h4>
-        <ul>
-          {
-            cars.map( car => {
-              return <CarItem  key={car.id} car={car} />
-              
-            })
-          }
-        </ul>
+        <button onClick={this.handleClick}>tocame</button>
+        <div onMouseMove= {this.handleMouseMove}
+          style={{border: '1px solid #000', marginTop: 10, padding:10}}>
+          <p>{this.state.mouseX},{this.state.mouseY}</p>
+          </div>
       </div>
 
     );
+  }
 }
-
-
 
 export default App;
